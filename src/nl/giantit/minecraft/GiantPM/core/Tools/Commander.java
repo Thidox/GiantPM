76e4f1faa3aa79ea3b4426a98f66e871a49b0792
Giant <giant@giantit.nl>
2012-04-13 02:34:49 +0200

    Slight modification of that neatness...

--- src/nl/giantit/minecraft/GiantPM/core/Tools/Commander.java
+++ src/nl/giantit/minecraft/GiantPM/core/Tools/Commander.java
@@ -12,15 +12,22 @@ import org.bukkit.entity.Player;
 public class Commander {
 	
 	public static boolean doCommand(Player p, String cmd, String arg) {
-		String[] args = {arg};
-		if(arg.contains(" ")) {
-		  args = arg.split(" ");
-		}
-		
 		if(cmd.equalsIgnoreCase("mute")) {
+			String[] args = {cmd,arg};
+			if(arg.contains(" ")) {
+				arg += cmd + " " + arg;
+				args = arg.split(" ");
+			}
+
 			mute.mute(p, args);
 			return true;
 		}else if(Misc.isEitherIgnoreCase(cmd, "um", "unmute")) {
+			String[] args = {cmd,arg};
+			if(arg.contains(" ")) {
+				arg += cmd + " " + arg;
+				args = arg.split(" ");
+			}
+
 			mute.unmute(p, args);
 			return true;
 		}/*else if(cmd.equalsIgnoreCase("join")) {
