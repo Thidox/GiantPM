9c439ea0257e12c216ca4bd1ac95eec92186f985
Giant <giant@giantit.nl>
2012-04-13 03:11:11 +0200

    I just loove changing changes

--- src/nl/giantit/minecraft/GiantPM/core/Tools/Commander.java
+++ src/nl/giantit/minecraft/GiantPM/core/Tools/Commander.java
@@ -5,6 +5,10 @@ import nl.giantit.minecraft.GiantPM.core.Commands.*;
 
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Giant
@@ -12,23 +16,27 @@ import org.bukkit.entity.Player;
 public class Commander {
 	
 	public static boolean doCommand(Player p, String cmd, String arg) {
+		List<String> args = new ArrayList<String>();
+		
 		if(cmd.equalsIgnoreCase("mute")) {
-			String[] args = {cmd,arg};
+			args.add(cmd);
 			if(arg.contains(" ")) {
-				arg += cmd + " " + arg;
-				args = arg.split(" ");
+				args.addAll(Arrays.asList(arg.split(" ")));
+			}else if(!arg.equals("")) {
+				args.add(arg);
 			}
 
-			mute.mute(p, args);
+			mute.mute(p, args.toArray(new String[args.size()]));
 			return true;
 		}else if(Misc.isEitherIgnoreCase(cmd, "um", "unmute")) {
-			String[] args = {cmd,arg};
+			args.add(cmd);
 			if(arg.contains(" ")) {
-				arg += cmd + " " + arg;
-				args = arg.split(" ");
+				args.addAll(Arrays.asList(arg.split(" ")));
+			}else if(!arg.equals("")) {
+				args.add(arg);
 			}
 
-			mute.unmute(p, args);
+			mute.unmute(p, args.toArray(new String[args.size()]));
 			return true;
 		}/*else if(cmd.equalsIgnoreCase("join")) {
 			
