702fe5794db6e4543475add7adaad938044d650b
Giant <giant@giantit.nl>
2012-04-18 01:16:51 +0200

    Heavy updates to the Channel so it is now working!

--- src/nl/giantit/minecraft/GiantPM/core/Tools/Channel/Channel.java
+++ src/nl/giantit/minecraft/GiantPM/core/Tools/Channel/Channel.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -16,20 +17,23 @@ import java.util.HashMap;
  */
 public class Channel {
 	
-	private static HashMap<Player, String> inChan = new HashMap<Player, String>();
+	private static HashMap<String, String> inChan = new HashMap<String, String>();
 	private static HashMap<String, Channel> channels = new HashMap<String, Channel>();
 	private Messages mH = GiantPM.getPlugin().getMsgHandler();
 	
-	private ArrayList<Player> members = new ArrayList<Player>();
+	private HashMap<String, Player> members = new HashMap<String, Player>();
+	private ArrayList<String> invited = new ArrayList<String>();
 	private int status = 1; //0 == open; 1 == private - inv allowed; 2 == private;
 	private String name;
-	private Player owner;
 	private perm perms = perm.Obtain();
 	
 	
 	private Channel(String name) {
 		this.name = name;
-		
+	}
+	
+	public boolean isOwner(Player p) {
+		return this.name.equalsIgnoreCase(p.getName());
 	}
 	
 	public void setStatus(String state) {
@@ -40,14 +44,16 @@ public class Channel {
 			HashMap<String, String> data = new HashMap<String, String>();
 			data.put("state", (tmp <= 0) ? "Public" : ((tmp >= 2) ? "Private" : "Private invitations allowed"));
 			
-			Heraut.say(owner, mH.getMsg(Messages.msgType.MAIN, "channelStateChanged", data));
+			Heraut.say(members.get(name), mH.getMsg(Messages.msgType.MAIN, "channelStateChanged", data));
 		}catch(NumberFormatException e) {
-			Heraut.say(owner, mH.getMsg(Messages.msgType.ERROR, "channelStateInvalid"));
+			Heraut.say(members.get(name), mH.getMsg(Messages.msgType.ERROR, "channelStateInvalid"));
 		}
 	}
 	
 	public void sendMsg(String msg) {
-		for(Player m : members) {
+		HashMap<String, Player> tmp = members; //Dirty lazy fix
+		for(Map.Entry<String, Player> t : tmp.entrySet()) {
+			Player m = t.getValue();
 			if(m.isOnline()) {
 				Heraut.say(m, msg);
 			}else{
@@ -62,11 +68,13 @@ public class Channel {
 	}
 	
 	public void sendMsg(Player p, String msg) {
-		for(Player m : members) {
+		HashMap<String, Player> tmp = members; //Dirty lazy fix
+		for(Map.Entry<String, Player> t : tmp.entrySet()) {
+			Player m = t.getValue();
 			if(m.isOnline()) {
 				Heraut.say(m, p.getDisplayName() + ": " + msg);
 			}else{
-				leaveChannel(m);
+				leaveChannel(m, true);
 				
 				HashMap<String, String> data = new HashMap<String, String>();
 				data.put("player", m.getDisplayName());
@@ -76,22 +84,38 @@ public class Channel {
 		}
 	}
 	
-	public ChannelResponse joinChannel(Player p, boolean inv) {
-		if(((status == 1 && !inv) || status == 2) && !perms.has(p, "giantpm.admin.fuckprivacy"))
+	public ChannelResponse joinChannel(Player p) {
+		if(((status == 1 && !invited.contains(p.getName())) || status == 2) && !perms.has(p, "giantpm.admin.fuckprivacy"))
 			return ChannelResponse.CHANNELISPRIVATE;
 		
 		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("player", p.getDisplayName());
 		
 		sendMsg(mH.getMsg(Messages.msgType.MAIN, "joinedChannel", data));
-		inChan.put(p, this.name);
-		members.add(p);
+		inChan.put(p.getName(), this.name);
+		members.put(p.getName(), p);
+		if(invited.contains(p.getName()))
+			invited.remove(p.getName());
+			
+		return ChannelResponse.CHANNELJOINED;
+	}
+	
+	public ChannelResponse joinChannel(Player p, boolean override) {
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("player", p.getDisplayName());
+		
+		sendMsg(mH.getMsg(Messages.msgType.MAIN, "joinedChannel", data));
+		inChan.put(p.getName(), this.name);
+		members.put(p.getName(), p);
+		if(invited.contains(p.getName()))
+			invited.remove(p.getName());
+			
 		return ChannelResponse.CHANNELJOINED;
 	}
 	
 	public void leaveChannel(Player p) {
-		inChan.remove(p);
-		members.remove(p);
+		inChan.remove(p.getName());
+		members.remove(p.getName());
 		
 		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("player", p.getDisplayName());
@@ -100,12 +124,50 @@ public class Channel {
 		Heraut.say(p, mH.getMsg(Messages.msgType.MAIN, "channelLeft"));
 	}
 	
+	public void leaveChannel(Player p, boolean quiet) {
+		inChan.remove(p.getName());
+		members.remove(p.getName());
+		
+		if(!quiet) {
+			HashMap<String, String> data = new HashMap<String, String>();
+			data.put("player", p.getDisplayName());
+
+			sendMsg(mH.getMsg(Messages.msgType.MAIN, "leftChannel", data));
+			Heraut.say(p, mH.getMsg(Messages.msgType.MAIN, "channelLeft"));
+		}
+	}
+	
+	public void addInvite(Player p) {
+		if(!invited.contains(p.getName()))
+			invited.add(p.getName());
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getMembersString() {
+		String s = "";
+		
+		int i = 0;
+		for(String pName : members.keySet()) {
+			if(i > 0) {
+				s += mH.getMsg(Messages.msgType.MAIN, "channelListCommaSeperator");
+			}
+			i++;
+			
+			s += pName;
+		}
+		
+		return s;
+	}
+	
 	public static boolean inChannel(Player p) {
-		return inChan.containsKey(p);
+		return inChan.containsKey(p.getName());
 	}
 	
 	public static String getPlayerChannelName(Player p) {
-		return inChan.get(p);
+		return inChan.get(p.getName());
 	}
 	
 	public static Channel getChannel(String cName) {
